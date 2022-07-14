32023bfb2501c6c252869c2fc6c794132f351708
Etnan Sousa <poketnans@gmail.com>
2022-07-13 22:30:53 -0300

    feat: implements Menu component

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,22 @@
+import { HStack, Center, Link } from "@chakra-ui/react";
+
+const Menu = () => {
+  return (
+    <HStack mt="100" color="gold" as="nav" gridGap="4">
+      <Center>
+        <Link href="/">Home</Link>
+      </Center>
+      <Center>
+        <Link href="/about-me">About Me</Link>
+      </Center>
+      <Center>
+        <Link href="/projects">Projects</Link>
+      </Center>
+      <Center>
+        <Link href="/contact">Contact</Link>
+      </Center>
+    </HStack>
+  );
+};
+
+export default Menu;
